9ac7fa69581d040f248a7f92ab86f5435142b6d9
Estefania <estefaniaballesteros201@gmail.com>
2023-08-24 13:36:53 -0500

    adicion del fetch sin arrays

--- src/components/views/OfertaInmobiliaria.jsx
+++ src/components/views/OfertaInmobiliaria.jsx
@@ -12,19 +12,19 @@ import { comunas, barrios, Year } from '../../data/inputSelects';
 import { InputAreas } from '../common/InputAreas';
 import { useEffect, useState } from 'react';
 import file from '../../assets/file.svg';
+import { fetchData } from '../../fetch/fetch';
 
 
 export function OfertaInmobiliaria() {
-    const initialStateFiltros = {tipoOferta: [], comuna: null, barrios: null, tipoPredio:[] , year: null, area_minima:null, area_maxima:null} /* declaracion del jason donde se guardaran los valores de acuerdo al onchange */
+    const initialStateFiltros = {tipoOferta: [], comuna: null, barrios: null, tipoPredio:[] , year: null, areaMinima:'', areaMaxima:''} /* declaracion del jason donde se guardaran los valores de acuerdo al onchange */
     const[filtros, setFiltros] = useState(initialStateFiltros) 
     const [showHola, setShowHola] = useState(false); // Estado para controlar la visibilidad de <h1>
 
     const handleChangeCheck= (e) => { //Evento para controlar el checkbox de tipo de oferta y tipo de predio
         let checkvalues = filtros[e.target.name]  //e obtiene el arreglo actual de valores seleccionados del estado filtros utilizando el nombre del checkbox como clave
-        console.log(e.target.name, checkvalues);
         if(checkvalues.includes(e.target.value)){ //verifica si el checkbox está marcado
             checkvalues = checkvalues.filter(value => value !== e.target.value) 
-            /* quí, el método filter se utiliza para crear un nuevo arreglo que excluye el valor que deseas remover (el valor del checkbox que se desmarca). value => value !== e.target.value es una función de filtro que excluye cualquier elemento que sea igual al valor del checkbox.Entonces, cuando el checkbox está marcado y se ejecuta este bloque, se está removiendo el valor del arreglo checkvalues. En esencia, esto significa que se está desmarcando la opción que ya estaba seleccionada. */
+            /* quí, el método filter se utiliza para crear un nuevo aregrlo que excluye el valor que deseas remover (el valor del checkbox que se desmarca). value => value !== e.target.value es una función de filtro que excluye cualquier elemento que sea igual al valor del checkbox.Entonces, cuando el checkbox está marcado y se ejecuta este bloque, se está removiendo el valor del arreglo checkvalues. En esencia, esto significa que se está desmarcando la opción que ya estaba seleccionada. */
         }else{
             checkvalues.push(e.target.value)
         }
@@ -41,9 +41,11 @@ export function OfertaInmobiliaria() {
     }
 
     const onHandleClickButton = () => { //Evento para controlar la vista desplegable de los resultados despues de dar click
-        console.log('holaa');
+        fetchData(filtros);
         setShowHola(true);
     };
+
+
     
     useEffect(() => {
         console.log(filtros);
@@ -80,8 +82,8 @@ export function OfertaInmobiliaria() {
         <div className="ContainerAreas">
             <label>Áreas</label>
             <div>
-                <InputAreas name="area_minima" value={filtros.area_minima} onChange={handleChange} placeholder= 'Mínimo' required={true}/>
-                <InputAreas name="area_maxima" value={filtros.area_maxima} onChange={handleChange} placeholder= 'Máximo' required={true}/>
+                <InputAreas name="area_minima" value={filtros.areaMinima} onChange={handleChange} placeholder= 'Mínimo' required={true}/>
+                <InputAreas name="area_maxima" value={filtros.areaMaxima} onChange={handleChange} placeholder= 'Máximo' required={true}/>
             </div>
         </div>
         <div className='contendeorBotones'>
@@ -94,8 +96,9 @@ export function OfertaInmobiliaria() {
             <div className='encabezado'>
                 <h3>Resultados de su consulta</h3>
                 <div>
-                    <img src={file} id='file'></img>
-                    <a href='https://www.google.com' target='blank' id='enlace'>Descargar informe</a></div>
+                    <img src={file} id='file' alt='Informe'></img>
+                    <a href='https://www.google.com' target='_blank' rel='noopener noreferrer' id='enlace'>Descargar informe</a>
+                </div>
             </div>
             <div className='ContainerScroll'>
                 <div className='description'>
@@ -111,47 +114,56 @@ export function OfertaInmobiliaria() {
                 </div>
                 <div className='containerTable FirstTable'>
                     <table>
-                        <tr>
-                            <th>Tipo de inmueble</th>
-                            <th>Promedio venta m<sup>2</sup></th>
-                            <th>Promedio arriendo m<sup>2</sup></th>
-                        </tr>
-                        <tr>
-                            <td>Apartamento</td>
-                            <td>2300</td>
-                            <td>104</td>
-                        </tr>
-                        <tr>
-                            <td>Casa</td>
-                            <td>2035</td>
-                            <td>54654</td>
-                        </tr>
+                        <thead>
+                            <tr>
+                                <th>Tipo de inmueble</th>
+                                <th>Promedio venta m<sup>2</sup></th>
+                                <th>Promedio arriendo m<sup>2</sup></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Apartamento</td>
+                                <td>2300</td>
+                                <td>104</td>
+                            </tr>
+                            <tr>
+                                <td>Casa</td>
+                                <td>2035</td>
+                                <td>54654</td>
+                            </tr>
+                        </tbody>
                     </table>
-                </div>    
+                </div>
                 <div className='descriptionTwo'>
                     <p>Otro ítem con información de valor</p>
                 </div>
                 <div className='containerTable'>
                     <table>
-                        <tr>
-                            <th>Tipo de inmueble</th>
-                            <th>Promedio venta m<sup>2</sup></th>
-                            <th>Promedio arriendo m<sup>2</sup></th>
-                        </tr>
-                        <tr>
-                            <td>Apartamento</td>
-                            <td>2300</td>
-                            <td>104</td>
-                        </tr>
-                        <tr>
-                            <td>Casa</td>
-                            <td>2035</td>
-                            <td>54654</td>
-                        </tr>
+                        <thead>
+                            <tr>
+                                <th>Tipo de inmueble</th>
+                                <th>Promedio venta m<sup>2</sup></th>
+                                <th>Promedio arriendo m<sup>2</sup></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Apartamento</td>
+                                <td>2300</td>
+                                <td>104</td>
+                            </tr>
+                            <tr>
+                                <td>Casa</td>
+                                <td>2035</td>
+                                <td>54654</td>
+                            </tr>
+                        </tbody>
                     </table>
-                </div>    
+                </div>
             </div>
         </div>
+
         } 
     </section> 
   )
---
+++ src/encode64.js
@@ -0,0 +1,5 @@
+import { Buffer } from "buffer";
+
+export default function encode64(dato) {
+  return Buffer.from(dato, "utf8").toString("base64");
+}
